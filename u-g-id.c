0b80543601e4820eca817c435e8f5b7611cac7fc
MarinaVer2525 <113004572+MarinaVer2525@users.noreply.github.com>
2025-04-06 12:08:04 +0700

    Update u-g-id.c

--- u-g-id.c
+++ u-g-id.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <unistd.h> // Для getuid() и getgid()
 
+int main() {
+    // Получаем UID и GID
+    uid_t uid = getuid();
+    gid_t gid = getgid();
+
+    // Выводим результат
+    printf("UID: %d\n", uid);
+    printf("GID: %d\n", gid);
+
+    return 0;
+}
